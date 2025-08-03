a4ec5d7b6b37ba6abb0d21132c6f4a84182422ab
tripau <Utkarsh.Tripathi5@cognizant.com>
2025-08-04 02:29:45 +0530

    new file:   src/main/java/com/rental/bike/entities/Customer.java

---
+++ src/main/java/com/rental/bike/entities/Customer.java
@@ -0,0 +1,24 @@
+package com.rental.bike.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phoneNumber;
+}
